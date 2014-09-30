af3657648698b06e6b63208c5c571f449a404878
Asgeir Sognefest <sogasg@gmail.com>
2014-09-30 22:55:31 +0200

    Try new message protocoll.

--- src/main/scala/com/cctrader/MasterActor.scala
+++ src/main/scala/com/cctrader/MasterActor.scala
@@ -27,7 +27,7 @@ class MasterActor extends Actor with ActorLogging {
 
   val config = ConfigFactory.load()
   val tableName = config.getString("instrumentTable")
-  val granularity = "_6hour"
+  val granularity = "_12hour"
 
   val dataActor = context.actorOf(Props[DataActor], "dataActor")
 
@@ -39,7 +39,6 @@ class MasterActor extends Actor with ActorLogging {
 
 
   //Classical
-/*
   context.actorOf(StochasticCoordinatorActor.props(dataActor, "tsSettings/classical/stochastic/Stochastic" + granularity +".conf"))
   context.actorOf(RSICoordinatorActor.props(dataActor, "tsSettings/classical/RSI/RSI" + granularity +".conf"))
   context.actorOf(ROCCoordinatorActor.props(dataActor, "tsSettings/classical/ROC/ROC" + granularity +".conf"))
@@ -48,6 +47,7 @@ class MasterActor extends Actor with ActorLogging {
   context.actorOf(AroonCoordinatorActor.props(dataActor, "tsSettings/classical/Aroon/Aroon" + granularity +".conf"))
   context.actorOf(MACDCoordinatorActor.props(dataActor, "tsSettings/classical/MACD/MACD" + granularity +".conf"))
 
+  /*
 
   //Classical Opposite
   context.actorOf(OppositeRSICoordinatorActor.props(dataActor, "tsSettings/classical/oppositeRSI/OppositeRSI" + granularity +".conf"))
@@ -66,6 +66,7 @@ class MasterActor extends Actor with ActorLogging {
   //context.actorOf(AroonCoordinatorActor.props(dataActor, "tsSettings/classical/Aroon/Aroon_2hour.conf"))
 
 
+  /*
   //6hour - TOP SYSTEMS
   context.actorOf(ROCCoordinatorActor.props(dataActor, "tsSettings/classical/ROC/ROC_6hour.conf"))
   context.actorOf(WilliamRCoordinatorActor.props(dataActor, "tsSettings/classical/WilliamR/WilliamR_6hour.conf"))
@@ -80,8 +81,7 @@ class MasterActor extends Actor with ActorLogging {
   context.actorOf(AroonCoordinatorActor.props(dataActor, "tsSettings/classical/Aroon/Aroon_12hour.conf"))
   context.actorOf(OppositeRSICoordinatorActor.props(dataActor, "tsSettings/classical/oppositeRSI/OppositeRSI_12hour.conf"))
   context.actorOf(MACDCoordinatorActor.props(dataActor, "tsSettings/classical/MACD/MACD_12hour.conf"))
-
-
+  */
 
 
   override def receive: Receive = {
